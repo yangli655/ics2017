f5ba86abab9b3971ae17930f21df58df2b3c3933
tracer-ics2017 <tracer@njuics.org>
2017-09-05 10:51:56 +0800

    >  compile U201414655 mosoire Linux mosoire 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  10:51:56 up  2:29,  1 user,  load average: 0.08, 0.10, 0.14 2e4e921dd4def6a31f581a8fa0914cefa0875c47

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -28,8 +28,8 @@ static struct rule {
   {"\\/", '/', 3},         // div
   {"==", TK_EQ, 7},        // equal
   {"!=", TK_NEQ, 7},       // not equal
-  {"&&", TK_AND, 11},       // and
-  {"||", TK_OR, 12} ,      // or
+  {"&&", TK_AND, 11},      // and
+  {"||", TK_OR, 12},       // or
   {"!", TK_NOT, 2},        // not
   {"\\(", TK_LP, 1},       // lp
   {"\\)", TK_RP, 1},       // rp
