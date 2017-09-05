aa8e1bce215b71cbf59156acdabf79a3286266f5
tracer-ics2017 <tracer@njuics.org>
2017-09-05 13:02:10 +0800

    >  compile U201414655 mosoire Linux mosoire 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  13:02:10 up 44 min,  1 user,  load average: 0.26, 0.17, 0.26 9d09718a45f3319ef64e30cc76c583fd8a44b49

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -128,11 +128,14 @@ static bool make_token(char *e) {
         break;
       }
     }
-
+    
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
     }
+  }
+  for(int j=0 ; j<nr_token;j++){
+    printf("%s\t%d\t",tokens[j].str,tokens[j].type);
   }
   return true;
 }
