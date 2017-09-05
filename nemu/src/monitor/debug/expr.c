78fe662332240d396e963607327c875ac57f8caf
tracer-ics2017 <tracer@njuics.org>
2017-09-05 23:32:04 +0800

    >  compile U201414655 mosoire Linux mosoire 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  23:32:04 up 11:14,  1 user,  load average: 0.15, 0.16, 0.12 b362eb13616281a9332a77f7711e6e9b5f54415

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -88,7 +88,8 @@ static bool make_token(char *e) {
          * of tokens, some extra actions should be performed.
          */
         switch (rules[i].token_type) {
-          case TK_NOTYPE: 
+          case TK_NOTYPE:
+            nr_token--;
             break;
           case '-':
             if(nr_token == 0 || (tokens[nr_token-1].type != TK_HEX && tokens[nr_token-1].type != TK_NUM && tokens[nr_token-1].type != TK_REG)) {
