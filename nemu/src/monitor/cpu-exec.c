aeafb7cdb2ae4e18845f9f0e6661872c3ea3a51d
tracer-ics2017 <tracer@njuics.org>
2017-09-05 17:41:44 +0800

    >  compile U201414655 mosoire Linux mosoire 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  17:41:44 up  5:24,  1 user,  load average: 0.47, 0.25, 0.20 e907f3296f3a433ccacdecacd884976ba1449338

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -31,6 +31,7 @@ void cpu_exec(uint64_t n) {
 #ifdef DEBUG
     /* TODO: check watchpoints here. */
     if(check_wp()) {
+      nemu_state=NEMU_STOP;
       printf("$eip: 0x%08x\n",cpu.eip);
     }
 
