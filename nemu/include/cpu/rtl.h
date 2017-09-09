30d8698519264e45341f1ba15292714dcc5e59ef
tracer-ics2017 <tracer@njuics.org>
2017-09-09 23:41:02 +0800

    >  compile U201414655 mosoire Linux mosoire 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  23:41:02 up 14:37,  1 user,  load average: 0.40, 0.33, 0.47 4a55214c62ff7052a586d469a68e47a9eb106c4

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -141,11 +141,11 @@ static inline void rtl_not(rtlreg_t* dest) {
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   //TODO();
-  printf("set:\t0x%08x\t",*src1);
+  printf("set:%d\t0x%08x\t",width,*src1);
   rtl_shli(dest,src1,(4-width)*8);
   printf("0x%08x\t",*dest);
   rtl_sari(dest,dest,(4-width)*8);
-  printf("0x%08x\t",*dest);
+  printf("0x%08x\n",*dest);
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
