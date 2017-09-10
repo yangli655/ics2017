acef080a51499dd6d634252fe4469dd85723a06d
tracer-ics2017 <tracer@njuics.org>
2017-09-10 19:25:36 +0800

    >  compile U201414655 mosoire Linux mosoire 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  19:25:36 up  9:48,  1 user,  load average: 0.03, 0.11, 0.15 2f0fcb2e214521c46b3213af8c910b8fb649137c

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -264,7 +264,6 @@ make_DHelper(a2O) {
 make_DHelper(J) {
   decode_op_SI(eip, id_dest, false);
   // the target address can be computed in the decode stage
-  printf("0x%08x\t%d\n",id_dest->simm,id_dest->simm);
   decoding.jmp_eip = id_dest->simm + *eip;
 }
 
