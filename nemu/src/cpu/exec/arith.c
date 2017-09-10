a1b10dbd51b8aedad93e9ce10281956d723b446e
tracer-ics2017 <tracer@njuics.org>
2017-09-10 14:39:59 +0800

    >  compile U201414655 mosoire Linux mosoire 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  14:39:59 up  5:02,  1 user,  load average: 0.58, 0.29, 0.18 bc1891a62952c42a9bf43ae77bce5d908999f61f

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -2,8 +2,28 @@
 
 make_EHelper(add) {
   //TODO();
-  rtl_addi(&reg_l(decoding.dest.reg),&reg_l(decoding.dest.reg),decoding.src.val);
-  rtl_update_ZFSF(&reg_l(decoding.dest.reg),decoding.dest.width);
+  //rtl_addi(&reg_l(decoding.dest.reg),&reg_l(decoding.dest.reg),decoding.src.val);
+  //rtl_update_ZFSF(&reg_l(decoding.dest.reg),decoding.dest.width);
+
+
+  rtl_add(&t2, &id_dest->val, &id_src->val);
+  //rtl_get_CF(&t1);
+  //rtl_add(&t2, &t2, &t1);
+  operand_write(id_dest, &t2);
+
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  rtl_sltu(&t0, &t2, &id_dest->val);
+  rtl_set_CF(&t0);
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_not(&t0);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
+
+
   print_asm_template2(add);
 }
 
