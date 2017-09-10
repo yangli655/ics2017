e474cb5d09b39bd469e98a1bcaad18e1bafcb379
tracer-ics2017 <tracer@njuics.org>
2017-09-10 23:06:27 +0800

    >  compile U201414655 mosoire Linux mosoire 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  23:06:27 up 13:29,  1 user,  load average: 0.32, 0.20, 0.14 5eb2de0658d198ec621b8dfdf4bebef76e3b67fe

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -66,8 +66,10 @@ make_EHelper(shl) {
   //TODO();
   // unnecessary to update CF and OF in NEMU
   rtl_lr(&t3,id_dest->reg,id_dest->width);
+  
   rtl_shli(&t3,&t3,reg_b(id_src->val));
   rtl_sr(id_dest->reg,id_dest->width,&t3);
+  printf("%x\t%d\n",t3,reg_b(id_src->val));
   rtl_update_ZFSF(&t3,id_dest->width);
 
   print_asm_template2(shl);
