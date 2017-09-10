564fcb69e2220ea1e2345d95394bb4ece599c30c
tracer-ics2017 <tracer@njuics.org>
2017-09-10 13:30:36 +0800

    >  compile U201414655 mosoire Linux mosoire 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  13:30:36 up  3:53,  1 user,  load average: 0.42, 0.36, 0.29 bbc4b0c8ea847dedbdbe7541bcce041fe494df63

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -115,7 +115,7 @@ static int cmd_info(char *args) {
     if (!strcmp(args, "r")) {
       printf("eax: 0x%08x\t\tecx: 0x%08x\t\tedx: 0x%08x\t\tebx: 0x%08x\n", cpu.eax, cpu.ecx, cpu.edx, cpu.ebx);
       printf("esp: 0x%08x\t\tebp: 0x%08x\t\tesi: 0x%08x\t\tedi: 0x%08x\n", cpu.esp, cpu.ebp, cpu.esi, cpu.edi);
-      printf("eip: 0x%08x\n", cpu.eip);
+      printf("eip: 0x%08x\t\teflags: 0x%08x\n", cpu.eip,cpu.EFLAGS);
     }
     else if (!strcmp(args, "w")) {
       print_wp();
