16cab8b81c64062097aa6051e76f1ba5bbe0b5c7
tracer-ics2017 <tracer@njuics.org>
2017-09-11 17:24:31 +0800

    >  compile U201414655 mosoire Linux mosoire 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  17:24:31 up  8:09,  1 user,  load average: 1.34, 0.68, 0.58 ff0d9eecf02a09d2f98595b656c1e7b8d9ae11a3

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -98,17 +98,17 @@ make_EHelper(movsx) {
 }
 
 make_EHelper(movzx) {
-  id_dest->width = 4;
+  id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
   rtl_zext(&t3,&id_src->val,2);
   operand_write(id_dest, &t3);
   print_asm_template2(movzx);
 }
 
 make_EHelper(movzb) {
-  id_dest->width = 2;
+  id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
   rtl_zext(&t3,&id_src->val,1);
   operand_write(id_dest, &t3);
-  print_asm_template2(movzb);
+  print_asm_template2(movzx);
 }
 
 make_EHelper(lea) {
