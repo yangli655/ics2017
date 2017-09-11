673a283032fcca6582a1cedcd25f81af7920470a
tracer-ics2017 <tracer@njuics.org>
2017-09-11 12:25:03 +0800

    >  compile U201414655 mosoire Linux mosoire 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  12:25:03 up  3:09,  1 user,  load average: 0.23, 0.14, 0.10 70edff86937e0de7df27f5e11157d31b91a4eba9

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -31,6 +31,16 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
       break;
     case CC_LE:
       break;
+    case CC_NL:
+      rtl_get_ZF(&t0);
+      rtl_neq0(&t0,&t0);
+      rtl_get_SF(&t1);
+      rtl_get_OF(&t2);
+      rtl_xor(&t1,&t1,&t2);
+      rtl_and(&t0,&t0,&t1);
+      rtl_mv(dest,&t0);
+      break;
+
       //TODO();
     default: panic("should not reach here");
       break;
