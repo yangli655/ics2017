99de6037a9450ed3e3ad33b0ec2b294d89f6b716
tracer-ics2017 <tracer@njuics.org>
2017-09-11 14:13:22 +0800

    >  compile U201414655 mosoire Linux mosoire 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  14:13:22 up  4:57,  1 user,  load average: 0.22, 0.13, 0.14 a507d25c4b68a110bec35beb42e1fd31203cca6d

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -91,7 +91,7 @@ opcode_entry opcode_table [512] = {
   /* 0x40 */	IDEX(r, inc), IDEX(r, inc), EMPTY, IDEX(r, inc),
   /* 0x44 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x48 */	IDEX(r, dec), EMPTY, EMPTY, EMPTY,
-  /* 0x4c */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x4c */	EMPTY, EMPTY, IDEX(r, dec), EMPTY,
   /* 0x50 */	IDEX(r, push), IDEX(r, push), IDEX(r, push), IDEX(r, push),
   /* 0x54 */	EMPTY, IDEX(r, push), IDEX(r, push), IDEX(r, push),
   /* 0x58 */	IDEX(r, pop), IDEX(r, pop), IDEX(r, pop), IDEX(r, pop),
