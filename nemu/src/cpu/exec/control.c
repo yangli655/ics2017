d767666c5d14e8a5ef44d3ddcedf49b9a481cc4f
tracer-ics2017 <tracer@njuics.org>
2017-09-11 16:00:37 +0800

    >  compile U201414655 mosoire Linux mosoire 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:00:37 up  6:45,  1 user,  load average: 0.44, 0.31, 0.25 89189eb0131c39d99443d5f23d5ab7d1bfc9d91d

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -43,10 +43,8 @@ make_EHelper(ret) {
 make_EHelper(call_rm) {
   //TODO();
   rtl_push(eip);
-  decoding.is_jmp = 1;
-
   rtl_lm(&decoding.jmp_eip,&id_dest->addr,id_dest->width);
-  //decoding.jmp_eip=id_dest->addr;
+  decoding.is_jmp = 1;
 
   print_asm("call *%s", id_dest->str);
 }
