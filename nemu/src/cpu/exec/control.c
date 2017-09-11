376667e51f0f50986acecdb507ad1ffd0af4e4c4
tracer-ics2017 <tracer@njuics.org>
2017-09-11 13:40:22 +0800

    >  compile U201414655 mosoire Linux mosoire 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  13:40:22 up  4:24,  1 user,  load average: 0.09, 0.14, 0.24 7615412bb8e422ef177fb8c57e519960f34f4b83

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -12,6 +12,7 @@ make_EHelper(jcc) {
   uint8_t subcode = decoding.opcode & 0xf;
   rtl_setcc(&t2, subcode);
   decoding.is_jmp = t2;
+  printf("%d\n",t2);
 
   print_asm("j%s %x", get_cc_name(subcode), decoding.jmp_eip);
 }
