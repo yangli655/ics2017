0db9072d8ac33992ec1cece03040f46540359136
tracer-ics2017 <tracer@njuics.org>
2017-09-14 23:33:33 +0800

    >  compile U201414655 mosoire Linux mosoire 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  23:33:33 up  1:59,  1 user,  load average: 0.17, 0.27, 0.35 459cfabf74957a8f79fbe19356f150aad0088dab

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -48,3 +48,5 @@ make_EHelper(call_rm);
 
 make_EHelper(in);
 make_EHelper(out);
+
+make_EHelper(neg);
--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -93,7 +93,18 @@ make_EHelper(dec) {
 }
 
 make_EHelper(neg) {
-  TODO();
+  //TODO();
+  if (id_dest->val == 0) {
+    rtl_li(&t3,0);
+  }
+  else {
+    rtl_li(&t3,1);
+  }
+  rtl_set_CF(&t3);
+  rtl_li(&t2,id_dest->val);
+  rtl_not(&t2);
+
+  operand_write(id_dest, &t2);
 
   print_asm_template1(neg);
 }
